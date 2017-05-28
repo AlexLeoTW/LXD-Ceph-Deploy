521c0d7ab01d543c5b92ef227ea288e5009c332b
AlexLeoTW <alex@alexleo.click>
2017-05-29 00:06:03 +0800

    not yet tested

---
+++ 2.ceph-deploy-tool.sh
@@ -0,0 +1,82 @@
+#! /bin/bash
+
+hosts=( )
+mon_hosts=( )
+deploy_user="ceph-deploy"
+deploy_user_pass=""
+
+# shellcheck source=config.sh
+source config.sh
+
+# test for sudo
+if [[ $UID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# create temp directory
+if [[ ! -d temp ]]; then
+   mkdir temp
+fi
+
+# ================= ceph host config =================
+                                                                                # create ceph-deploy user
+echo "$(title "ceph-deploy-tool" "create ceph-deploy user [$deploy_user] in each host")"
+
+for (( i=0; i<${#hosts}; i++ )); do
+    # adduser --disabled-password --gecos "" $deploy_user
+    lxc exec ${hosts[i]} -- adduser --disabled-password --gecos "" $deploy_user
+    # echo "$deploy_user_pass" | passwd "$deploy_user" --stdin
+    lxc exec ${hosts[i]} -- echo "$deploy_user_pass" | passwd "$deploy_user" --stdin
+    # adduser $deploy_user sudo
+    lxc exec ${hosts[i]} -- adduser $deploy_user sudo
+    # echo "${deploy_user} ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/${deploy_user}
+    lxc exec ${hosts[i]} -- echo "${deploy_user} ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/${deploy_user}
+    # sudo chmod 0440 /etc/sudoers.d/${deploy_user}
+    lxc exec ${hosts[i]} -- chmod 0440 /etc/sudoers.d/${deploy_user}
+done
+
+# ==================================
+
+echo "$(title "ceph-deploy-tool" "push config.sh")"                             # push config.sh
+
+cp config.sh temp/config.sh
+sed -i "/deploy_user_pass/d" temp/config.sh
+for (( i = 0; i < ${#hosts}; i++ )); do
+    lxc push temp/config.sh ${hosts[i]}/home/$deploy_user/config.sh
+done
+rm temp/config.sh
+
+# ==================================
+
+echo "$(title "ceph-deploy-tool" "config ceph repo")"                           # launch ceph preflight
+
+for (( i = 0; i < ${#hosts}; i++ )); do
+    lxc file push files/ceph_install_1_preflight.sh ${hosts[i]}/home/$deploy_user/my-cluster/ceph_install_1_preflight.sh
+    lxc exec ${hosts[i]} -- chmod +x /home/$deploy_user/ceph_install_1_preflight.sh
+    lxc exec ${hosts[i]} -- /home/$deploy_user/ceph_install_1_preflight.sh
+done
+
+# ==================================
+
+echo "$(title "ceph-deploy-tool" "distrobute ssh keys")"                        # distrobute ssh keys
+
+for (( index=0; index<${#mon_hosts[@]}; index++ )); do
+
+    if [[ -f /home/$deploy_user/.ssh/id_rsa ]]; then
+        lxc exec ${mon_hosts[index]} -- ssh-keygen -t rsa -N "" -f /home/$deploy_user/.ssh/id_rsa
+    fi
+
+    lxc file pull ${mon_hosts[index]}/home/$deploy_user/.ssh/id_rsa.pub temp/${mon_hosts[index]}.hostkey.pub
+
+    for (( i=0; i<${#hosts[@]}; i++ )); do
+        lxc file push temp/${mon_hosts[index]}.hostkey.pub ${hosts[i]}/home/$deploy_user/deplyHost.key
+        lxc exec ${hosts[i]} -- cat /home/$deploy_user/deplyHost.key >> /home/$deploy_user/.ssh/authorized_keys
+        lxc exec ${hosts[i]} -- rm /home/$deploy_user/deplyHost.key
+    done
+
+    rm temp/${mon_hosts[index]}.hostkey.pub
+
+done
+
+# ==================================
--- files/ceph_install_1_preflight.sh
+++ files/ceph_install_1_preflight.sh
@@ -3,6 +3,9 @@
 deploy_user="ceph-deploy"
 source config.sh
 
+apt install ntp
+apt install openssh-server
+
 apt purge ceph ceph-common ceph-dbg ceph-deploy ceph-fs-common ceph-fuse ceph-mds
 apt autoremove
 
@@ -11,8 +14,6 @@ echo deb https://download.ceph.com/debian-kraken/ "$(lsb_release -sc)" main | te
 apt update
 apt install ceph-deploy
 
-apt install ntp
-apt install openssh-server
 
 # config firewell
 apt install ufw
@@ -20,13 +21,6 @@ ufw allow ssh
 ufw allow 6789/tcp
 ufw allow 6800:7300/tcp
 
-
-adduser --disabled-password --gecos "" $deploy_user
-adduser $deploy_user sudo
-echo "${deploy_user} ALL = (root) NOPASSWD:ALL" | tee /etc/sudoers.d/${deploy_user}
-sudo chmod 0440 /etc/sudoers.d/${deploy_user}
-
 # running as deploy_user
 su $deploy_user
-
 ssh-keygen -t rsa -N "" -f /home/$deploy_user/.ssh/id_rsa
