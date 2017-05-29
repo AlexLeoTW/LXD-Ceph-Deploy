32f57a24700209d6cff576861cc90330ed824e6e
AlexLeoTW <alex@alexleo.click>
2017-05-29 22:06:07 +0800

    not yet tested build-cluster.sh

---
+++ 3.build-cluster.sh
@@ -0,0 +1,99 @@
+#! /bin/bash
+
+mon_hosts=( )
+mon_ip=( )
+osd_hosts=( )
+osd_disks=( )
+mds_hosts=( )
+
+# shellcheck source=config.sh
+source config.sh
+# shellcheck source=files/functions.sh
+source files/functions.sh
+
+# test for sudo
+if [[ $UID -ne 0 ]]; then
+   echo "This script must be run as root"
+   exit 1
+fi
+
+# create temp directory
+if [[ ! -d temp ]]; then
+   mkdir temp
+fi
+
+# ================= build ceph cluster =================
+
+title "build-ceph-cluster" "build ceph.conf"                                    # build ceph.conf
+
+rm temp/ceph.conf*
+cp templates/ceph_conf_template temp/ceph.conf
+sed -i "s/%new_fsid%/$(uuidgen)/g" temp/ceph.conf
+
+mon_list="${mon_hosts[0]}"
+mon_ip_list="${mon_ip[0]}"
+for (( i=1; i<${#mon_hosts[@]}; i++ )); do
+   mon_list+=", ${mon_hosts[i]}"
+   mon_ip_list+=", ${mon_ip[i]}"
+done
+
+sed -i -- "s/%mon_hosts%/${mon_list}/g" temp/ceph.conf
+sed -i -- "s/%mon_ip%/${mon_ip_list}/g" temp/ceph.conf
+sed -i -- "s/%osd_size%/${#osd_hosts[@]}/g" temp/ceph.conf
+
+for (( i=0; i<${#mon_hosts[@]}; i++ )); do
+   cat templates/ceph_config_entry_mon_template >> temp/ceph.conf.mon
+   sed -i "s/%hostname%/${mon_hosts[i]}/g" temp/ceph.conf.mon
+   sed -i "s/%host_ip%/${mon_ip[i]}/g" temp/ceph.conf.mon
+done
+
+for (( i=0; i<${#osd_hosts[@]}; i++ )); do
+   cat templates/ceph_config_entry_template >> temp/ceph.conf.osd
+   sed -i "s/%type%/osd/g" temp/ceph.conf.osd
+   sed -i "s/%hostname%/${osd_hosts[i]}/g" temp/ceph.conf.osd
+done
+
+for (( i=0; i<${#mds_hosts[@]}; i++ )); do
+    cat templates/ceph_config_entry_template >> temp/ceph.conf.mds
+    sed -i "s/%type%/mds/g" temp/ceph.conf.mds
+    sed -i "s/%hostname%/${mds_hosts[i]}/g" temp/ceph.conf.mds
+done
+
+lineReplace temp/ceph.conf %mon_hosts_detial% "$(cat temp/ceph.conf.mon)"
+lineReplace temp/ceph.conf %osd_hosts_detial% "$(cat temp/ceph.conf.osd)"
+lineReplace temp/ceph.conf %mds_hosts_detial% "$(cat temp/ceph.conf.mds)"
+
+rm ceph.conf.mon
+rm ceph.conf.osd
+rm ceph.conf.mds
+
+# ==================================
+
+title "ceph host config" "mapping physical drives"                              # mapping physical drives
+for (( i=0; i<${#osd_hosts[@]}; i++ )); do
+  lxc stop ${osd_hosts[i]}
+  lxc config device add ${osd_hosts[i]} sda unix-block path=${osd_disks[i]}
+  lxc start ${osd_hosts[i]}
+done
+
+# ==================================
+
+title "build-ceph-cluster" "really deploying cluster"                           # build ceph.conf
+
+echo "installing ceph package for all lxc hosts"
+lxc exec ${mon_hosts[0]} -- ceph-deploy install "${mon_hosts[@]}"
+
+echo "add the initial monitor(s) and gather the keys"
+lxc exec ${mon_hosts[0]} -- ceph-deploy mon create-initial
+
+for (( i=0; i<${#osd_hosts[@]}; i++ )); do
+    echo "build osd ${osd_hosts[i]}:${osd_disks[i]}"
+    lxc exec ${mon_hosts[0]} -- ceph-deploy disk zap ${osd_hosts[i]}:${osd_disks[i]}
+    ceph-deploy osd create ${osd_hosts[i]}:${osd_disks[i]}
+done
+
+lxc exec ${mon_hosts[0]} -- ceph-deploy admin ${mon_hosts[0]}
+
+lxc exec ${mon_hosts[0]} -- chmod +r /etc/ceph/ceph.client.admin.keyring
+
+lxc exec ${mon_hosts[0]} -- ceph health
--- files/functions.sh
+++ files/functions.sh
@@ -2,16 +2,16 @@
 
 # (title, sub-title)
 function title() {
-   printf '\n'
-   printf '\n'
-   printf "===============================================\n"
-   printf "  %s\n" "$1"
-   if [[ $2 != '' ]]; then
-      printf "    - %s\n" "$2"
-   fi
-   printf "===============================================\n"
-   printf '\n'
-   printf '\n'
+    printf '\n'
+    printf '\n'
+    printf "===============================================\n"
+    printf "  %s\n" "$1"
+    if [[ $2 != '' ]]; then
+        printf "    - %s\n" "$2"
+    fi
+    printf "===============================================\n"
+    printf '\n'
+    printf '\n'
 }
 
 # (file, target, lines)
@@ -23,4 +23,17 @@ function lineReplace() {
 
     cp temp/lineReplace  $1
     rm temp/lineReplace
+}
+
+# (array, delimiter)
+function arrayToString() {
+    declare -a array=("${!1}")
+    result=''
+
+    for (( i=0; i<${#array[@]}-1; i++ )); do
+        result+="${array[i]}$2"
+    done
+    result+="${array[i]}"
+
+    echo $result
 }
