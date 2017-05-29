bb64e91fecd9ca01cea3290dfeb56f9f63b32e72
AlexLeoTW <alex@alexleo.click>
2017-05-29 22:35:36 +0800

    remove old files

--- test.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-osd_hosts=( osd0 osd1 )
-osd_disks=( "/dev/sda" "/dev/sdc" )
-
-for (( i=0; i<${#osd_hosts[@]}; i++ ));
-do
-  echo lxc config device add ${osd_hosts[i]} sda unix-block path=${osd_disks[i]}
-done
