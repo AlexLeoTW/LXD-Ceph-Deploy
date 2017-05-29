9566245bb1e67e104c5aa0f9636b882311ebb68e
AlexLeoTW <alex@alexleo.click>
2017-05-29 22:52:31 +0800

    remove old files

--- my-cluster/2_mk_config.sh
+++
@@ -1,7 +0,0 @@
-echo "backup old config"
-mv ceph.conf ceph.conf.bk
-
-echo "[global]" >> ceph.conf
-echo "fsid = $(uuidgen)" >> ceph.conf
-echo "fsid = $(uuidgen)"
-cat ceph.conf.templete >> ceph.conf
